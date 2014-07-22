e427362858325e349b9f1b1d41852dda902641e5
tylers <tylers@nationalfibre.net>
2014-07-22 09:47:02 -0400

    localizing art views

--- workbench/rtoya/art/src/views/art.blade.php
+++ workbench/rtoya/art/src/views/art.blade.php
@@ -2,11 +2,25 @@
 
 @section('content')
 
-Artist: {{ $artist->name }}<p>
-Name: {{ $art->name }}<br>
-Photos:<br>
-@foreach ($art->photos as $photo)
-&nbsp;&nbsp;&nbsp;&nbsp;{{ $photo->path }}<br>
-@endforeach
+<div class="row">
+    <div class="small-12">
+        <h2>Art</h2>
+    </div>
+</div>
+
+<div class="row">
+    <div class="columns small-12 medium-3">
+        @include('art::navigation-main')
+    </div>
+
+    <div class="columns small-12 medium-9">
+        Artist: {{ $artist->name }}<p>
+        Name: {{ $art->name }}<br>
+        Photos:<br>
+        @foreach ($art->photos as $photo)
+        &nbsp;&nbsp;&nbsp;&nbsp;{{ $photo->path }}<br>
+        @endforeach
+    </div>
+</div>
 
 @stop
--- workbench/rtoya/art/src/views/index.blade.php
+++ workbench/rtoya/art/src/views/index.blade.php
@@ -2,10 +2,22 @@
 
 @section('content')
 
-Art!<p>
+<div class="row">
+    <div class="small-12">
+        <h2>{{ Lang::get('art::labels.art') }} - {{ Lang::get('art::labels.art.featured-arts') }}</h2>
+    </div>
+</div>
 
-@foreach ($featuredArts as $featuredArt)
-Name: {{ $featuredArt->name }}<br>
-@endforeach
+<div class="row">
+    <div class="columns small-12 medium-3">
+        @include('art::navigation-main')
+    </div>
+
+    <div class="columns small-12 medium-9">
+        @foreach ($featuredArts as $featuredArt)
+        Name: {{ $featuredArt->name }}<br>
+        @endforeach
+    </div>
+</div>
 
 @stop
