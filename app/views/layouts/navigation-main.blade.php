174c61622976a8c1b56697c1ed844de03c1d080b
tylers <tylers@nationalfibre.net>
2014-07-22 00:16:05 -0400

    main navigation links

--- app/views/layouts/navigation-main.blade.php
+++ app/views/layouts/navigation-main.blade.php
@@ -2,6 +2,6 @@
     <ul>
         <li>{{ HTML::linkRoute('dashboard', 'Dashboard') }}</li>
         <li>{{ HTML::linkRoute('account', 'Account Settings') }}</li>
-        <li>{{ HTML::linkRoutE('my-arts', 'My Arts') }}</li>
+        <li>{{ HTML::linkRoutE('art', 'Arts') }}</li>
     </ul>
 </div>
